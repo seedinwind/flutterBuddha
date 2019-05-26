e98e422d2dadef687d7e102b1932db1cd99496d7
seedinwind <1211615659@qq.com>
2019-05-27 01:23:39 +0800

    first commit

---
+++ lib/playerpage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class PlayerPage extends StatefulWidget {
+  var url;
+
+  PlayerPage({url: String}) {
+    this.url = url;
+  }
+
+  @override
+  _PlayerState createState() {
+    return _PlayerState();
+  }
+}
+
+class _PlayerState extends State<PlayerPage> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.url)
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+        ));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+}
